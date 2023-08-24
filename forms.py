2908b9a0f9043b2b68f4c8d94ca17435aa3eb5ce
BinduBhatoy <kumarravinder6245@gmail.com>
2023-08-24 21:29:20 +0530

    set virtual env

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.html5 import EmailField
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
